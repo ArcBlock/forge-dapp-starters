74d5e3126567f4e18280ff8c3a5b2948e562aa1e
wangshijun <wangshijun2010@gmail.com>
2019-06-10 19:07:10 +0800

    chore: sync code

--- packages/forge-gatsby-starter/api/libs/env.js
+++ packages/forge-gatsby-starter/api/libs/env.js
@@ -7,6 +7,11 @@ module.exports = {
     process.env.chainHost,
   appId: process.env.REACT_APP_APP_ID || process.env.GATSBY_APP_ID || process.env.APP_ID || process.env.appId,
   appName: process.env.REACT_APP_APP_NAME || process.env.GATSBY_APP_NAME || process.env.APP_NAME || process.env.appName,
+  appDescription:
+    process.env.REACT_APP_APP_DESCRIPTION ||
+    process.env.GATSBY_APP_DESCRIPTION ||
+    process.env.APP_DESCRIPTION ||
+    process.env.appDescription,
   baseUrl: process.env.REACT_APP_BASE_URL || process.env.GATSBY_BASE_URL || process.env.BASE_URL || process.env.baeUrl,
   apiPrefix:
     process.env.REACT_APP_API_PREFIX ||
