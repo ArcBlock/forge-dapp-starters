9627924580fa4129829e957a30f6e6539c71767c
wangshijun <wangshijun2010@gmail.com>
2019-06-16 16:40:02 +0800

    chore: update keystone starter

--- packages/forge-keystone-starter/api/routes/auth/login.js
+++ packages/forge-keystone-starter/api/routes/auth/login.js
@@ -1,13 +1,15 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable no-console */
+const keystone = require('keystone');
 const env = require('../../libs/env');
-const { User } = require('../../models');
 const { login } = require('../../libs/jwt');
 
 const description = {
   en: `Login ${env.appName} with your ABT Wallet`,
   zh: `用 ABT 钱包登录 ${env.appName}`,
 };
+
+const User = keystone.list('User').model;
 
 module.exports = {
   action: 'login',
@@ -17,19 +19,19 @@ module.exports = {
       description: description[locale] || description.en,
     }),
   },
-  onAuth: async ({ claims, did, token, storage }) => {
+  onAuth: async ({ claims, userDid, token, storage }) => {
     try {
       const profile = claims.find(x => x.type === 'profile');
-      const exist = await User.findOne({ did });
+      const exist = await User.findOne({ did: userDid });
       if (exist) {
-        console.log('update user', did, JSON.stringify(profile));
+        console.log('update user', userDid, JSON.stringify(profile));
         exist.name = profile.fullName;
         exist.email = profile.email;
         await exist.save();
       } else {
-        console.log('create user', did, JSON.stringify(profile));
+        console.log('create user', userDid, JSON.stringify(profile));
         const user = new User({
-          did,
+          did: userDid,
           name: profile.fullName,
           email: profile.email,
         });
@@ -37,9 +39,9 @@ module.exports = {
       }
 
       // Generate new session token that client can save to localStorage
-      const sessionToken = await login(did);
-      await storage.update(token, { did, sessionToken });
-      console.error('login.onAuth.login', { did, sessionToken });
+      const sessionToken = await login(userDid);
+      await storage.update(token, { did: userDid, sessionToken });
+      console.error('login.onAuth.login', { userDid, sessionToken });
     } catch (err) {
       console.error('login.onAuth.error', err);
     }
