caea1d6aa85e428aff3ebde8191694690eef9c59
wangshijun <wangshijun2010@gmail.com>
2019-06-18 17:48:57 +0800

    chore: add bundle analyzer

--- packages/forge-next-starter/src/next.config.js
+++ packages/forge-next-starter/src/next.config.js
@@ -4,42 +4,58 @@ require('dotenv').config();
 const path = require('path');
 const withCSS = require('@zeit/next-css');
 const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
 module.exports = withCSS(
-  withCustomBabelConfigFile({
-    babelConfigFile: path.join(__dirname, './babel.config.js'),
+  withCustomBabelConfigFile(
+    withBundleAnalyzer({
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../dist/server.html',
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../dist/client.html',
+        },
+      },
 
-    env: {
-      appName: process.env.APP_NAME,
-      appId: process.env.APP_ID,
-      apiPrefix: process.env.NF_API_PREFIX || process.env.API_PREFIX || '',
-      chainHost: process.env.CHAIN_HOST,
-      chainId: process.env.CHAIN_ID,
-    },
+      babelConfigFile: path.join(__dirname, './babel.config.js'),
 
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty',
-      };
+      env: {
+        appName: process.env.APP_NAME,
+        appId: process.env.APP_ID,
+        apiPrefix: process.env.NF_API_PREFIX || process.env.API_PREFIX || '',
+        chainHost: process.env.CHAIN_HOST,
+        chainId: process.env.CHAIN_ID,
+      },
 
-      const originalEntry = config.entry;
-      config.entry = async () => {
-        const entries = await originalEntry();
-        if (entries['main.js']) {
-          entries['main.js'].unshift('@babel/polyfill');
-        }
-        return entries;
-      };
+      webpack: config => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+          fs: 'empty',
+        };
 
-      // fixes https://github.com/graphql/graphql-js/issues/1272
-      config.module.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      });
+        const originalEntry = config.entry;
+        config.entry = async () => {
+          const entries = await originalEntry();
+          if (entries['main.js']) {
+            entries['main.js'].unshift('@babel/polyfill');
+          }
+          return entries;
+        };
 
-      return config;
-    },
-  })
+        // fixes https://github.com/graphql/graphql-js/issues/1272
+        config.module.rules.push({
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto',
+        });
+
+        return config;
+      },
+    })
+  )
 );
