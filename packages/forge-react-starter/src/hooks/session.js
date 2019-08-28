4bdb5f56ae26e7732aa7853da08f06a82362a664
wangshijun <wangshijun2010@gmail.com>
2019-08-28 15:58:39 +0800

    chore: move token info to session

--- packages/forge-react-starter/src/hooks/session.js
+++ packages/forge-react-starter/src/hooks/session.js
@@ -1,14 +1,21 @@
 import useAsync from 'react-use/lib/useAsync';
 import api from '../libs/api';
+import forge from '../libs/sdk';
 import { removeToken } from '../libs/auth';
 
 async function fetchSession() {
   try {
-    const res = await api.get('/api/session');
-    if (res.status === 400) {
+    const [{ status, data }, { state }] = await Promise.all([
+      api.get('/api/session'),
+      forge.getForgeState({}, { ignoreFields: ['state.protocols', /\.txConfig$/, /\.gas$/] }),
+    ]);
+
+    if (status === 400) {
       removeToken();
     }
-    return res.data;
+
+    data.token = state.token;
+    return data;
   } catch (err) {
     removeToken();
   }
