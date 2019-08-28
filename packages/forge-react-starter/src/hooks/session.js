303f0724bb2da0c30d1fb68151c3970366531dda
wangshijun <wangshijun2010@gmail.com>
2019-08-28 16:14:20 +0800

    fix: token symbol and decimal should be consistent with online version

--- packages/forge-react-starter/src/hooks/session.js
+++ packages/forge-react-starter/src/hooks/session.js
@@ -1,20 +1,15 @@
 import useAsync from 'react-use/lib/useAsync';
 import api from '../libs/api';
-import forge from '../libs/sdk';
 import { removeToken } from '../libs/auth';
 
 async function fetchSession() {
   try {
-    const [{ status, data }, { state }] = await Promise.all([
-      api.get('/api/session'),
-      forge.getForgeState({}, { ignoreFields: ['state.protocols', /\.txConfig$/, /\.gas$/] }),
-    ]);
+    const { status, data } = await api.get('/api/session');
 
     if (status === 400) {
       removeToken();
     }
 
-    data.token = state.token;
     return data;
   } catch (err) {
     removeToken();
