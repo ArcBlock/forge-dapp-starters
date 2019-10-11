759a4e538133fdd3ce3f05af4b770ee26ecc1fa3
wangshijun <wangshijun2010@gmail.com>
2019-10-11 11:56:58 +0800

    chore: use latest forge sdk and ux lib

--- packages/forge-gatsby-starter/templates/api/routes/session.js
+++ packages/forge-gatsby-starter/templates/api/routes/session.js
@@ -3,11 +3,25 @@ const ForgeSDK = require('@arcblock/forge-sdk');
 module.exports = {
   init(app) {
     app.get('/api/session', async (req, res) => {
-      const { state } = await ForgeSDK.getForgeState(
-        {},
-        { ignoreFields: ['state.protocols', /\.txConfig$/, /\.gas$/] }
-      );
-      res.json({ user: req.user, token: state.token });
+      const { getForgeState: data } = await ForgeSDK.doRawQuery(`{
+          getForgeState {
+            code
+            state {
+              token {
+                decimal
+                description
+                icon
+                inflationRate
+                initialSupply
+                name
+                symbol
+                totalSupply
+                unit
+              }
+            }
+          }
+        }`);
+      res.json({ user: req.user, token: data.state.token });
     });
 
     app.post('/api/logout', (req, res) => {
